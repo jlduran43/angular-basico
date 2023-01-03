3792e51a90f923a05e6cb87a3ecbd0d5c8d0d13f
jduranc <jduranc@bch.bancodechile.cl>
2023-01-02 23:25:27 -0300

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,17 @@
+import { ContadorComponent } from "./contador/contador.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [
+        ContadorComponent
+    ],
+    imports: [
+    ],
+    exports: [
+        ContadorComponent
+    ]
+})
+
+export class ContadorModule{
+
+}
